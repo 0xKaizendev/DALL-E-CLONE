b06ce122ad7d00f637511a0abefe30132a07b50d
0xZales <rozalesamiral@gmail.com>
2023-02-02 03:04:11 +0000

    bh

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 // CONFIG
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
+ const PORT = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
